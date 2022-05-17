32c441806be18e3b427047f896e339e2eee96517
carlruan <62496966+carlruan@users.noreply.github.com>
2022-05-17 18:34:01 -0400

    may 17

---
+++ Train_with_JAVA/basic/ch2/kmp/Kmp.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Kmp {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(reader.readLine());
+        String p = reader.readLine();
+        p = "!" + p;
+        int m = Integer.parseInt(reader.readLine());
+        String s = reader.readLine();
+        s = "?" + s;
+        int[] ne = new int[n + 1];
+        for (int i = 2, j = 0; i < n + 1; i++) {
+            while (j > 0 && p.charAt(i) != p.charAt(j + 1))
+                j = ne[j];
+            if (p.charAt(i) == p.charAt(j + 1))
+                j++;
+            ne[i] = j;
+        }
+
+        for (int i = 1, j = 0; i < m + 1; i++) {
+            while (j > 0 && p.charAt(j + 1) != s.charAt(i))
+                j = ne[j];
+
+            if (p.charAt(j + 1) == s.charAt(i))
+                j++;
+
+            if (j == n) {
+                writer.write(i - n + " ");
+                ;
+                j = ne[j];
+            }
+        }
+        writer.flush();
+        writer.close();
+        reader.close();
+    }
+}
---
+++ Train_with_JAVA/basic/ch2/math/Math.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Math {
+
+    private static Stack<Integer> num = new Stack<>();
+    private static Stack<Character> op = new Stack<>();
+
+    private static void cal() {
+        int n2 = num.pop();
+        int n1 = num.pop();
+        char c = op.pop();
+        if (c == '+')
+            num.push(n1 + n2);
+        else if (c == '-')
+            num.push(n1 - n2);
+        else if (c == '*')
+            num.push(n1 * n2);
+        else
+            num.push(n1 / n2);
+    }
+
+    private static int getRank(char c) {
+        if (c == '+' || c == '-')
+            return 1;
+        if (c == '(')
+            return 0;
+        return 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                int cur = c - '0';
+                while (i + 1 < n && Character.isDigit(s.charAt(i + 1))) {
+                    cur = cur * 10 + s.charAt(++i) - '0';
+                }
+                num.push(cur);
+            } else if (c == '(') {
+                op.push(c);
+            } else if (c == ')') {
+                while (op.peek() != '(') {
+                    cal();
+                }
+                op.pop();
+            } else {
+                while (!op.isEmpty() && getRank(op.peek()) >= getRank(c)) {
+                    cal();
+                }
+                op.push(c);
+            }
+        }
+        while (!op.isEmpty())
+            cal();
+        System.out.println(num.pop());
+
+    }
+}
