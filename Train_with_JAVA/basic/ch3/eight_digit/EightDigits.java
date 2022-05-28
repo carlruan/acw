07723426def1e775da6b56f5ad274feed2a1966e
carlruan <62496966+carlruan@users.noreply.github.com>
2022-05-28 15:15:04 -0400

    may28

---
+++ Train_with_JAVA/basic/ch3/eight_digit/EightDigits.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+class EightDigits {
+    static void swap(char[] a, int i, int j) {
+        char tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    static int bfs(String start, String end) {
+        Map<String, Integer> map = new HashMap<>();
+        Queue<String> q = new LinkedList<>();
+        q.offer(start);
+        map.put(start, 0);
+
+        int[] dx = { -1, 0, 1, 0 }, dy = { 0, 1, 0, -1 };
+        while (!q.isEmpty()) {
+            String s = q.poll();
+            if (s.equals(end))
+                return map.get(s);
+
+            // 获得x的位置
+            int k = s.indexOf('x');
+            int x = k / 3, y = k % 3;
+            for (int i = 0; i < 4; i++) {
+                int a = x + dx[i], b = y + dy[i];
+                if (a < 3 && a >= 0 && b < 3 && b >= 0) {
+                    // 对字符串中的字符进行更改
+                    char[] arr = s.toCharArray();
+                    swap(arr, k, a * 3 + b);
+                    String str = new String(arr);
+                    // 如果没有被遍历过
+                    if (map.get(str) == null) {
+                        map.put(str, map.get(s) + 1);
+                        q.offer(str);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = cin.readLine().split(" ");
+        String start = "";
+        for (int i = 0; i < s.length; i++)
+            start += s[i];
+        String end = "12345678x";
+        System.out.println(bfs(start, end));
+    }
+}
---
+++ Train_with_JAVA/basic/ch3/puzzle/puzzle.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class puzzle {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt(), n = scan.nextInt();
+        int[][] matrix = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] { 1, 1 });
+        int res = 0;
+        int[] dx = new int[] { 0, -1, 0, 1 };
+        int[] dy = new int[] { 1, 0, -1, 0 };
+        while (!q.isEmpty()) {
+            for (int i = q.size(); i > 0; i--) {
+                int[] cur = q.poll();
+
+                if (cur[0] == m && cur[1] == n) {
+                    System.out.println(res);
+                    return;
+                }
+                for (int d = 0; d < 4; d++) {
+                    int nx = cur[0] + dx[d];
+                    int ny = cur[1] + dy[d];
+                    // System.out.println(nx + " " + ny);
+                    if (nx < 1 || nx > m || ny < 1 || ny > n || matrix[nx][ny] == 1)
+                        continue;
+                    matrix[nx][ny] = 1;
+                    q.add(new int[] { nx, ny });
+
+                }
+            }
+            res++;
+        }
+    }
+}
